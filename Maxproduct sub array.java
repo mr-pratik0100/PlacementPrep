a67e624b5dc89518f20ce3973642d4fd5b298c0d
pratik deepak shiradkar <108323902+mr-pratik0100@users.noreply.github.com>
2023-04-30 23:07:33 +0530

    Create Maxproduct sub array.java

---
+++ Maxproduct sub array.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prod =1;
+        int maxProd =Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            prod=prod*nums[i];
+            if(prod>maxProd){
+                maxProd=prod;
+            }
+            if(prod==0){
+                prod=1;
+            }
+        }
+         prod=1;
+    
+    for(int i=nums.length-1;i>=0;i--){
+            prod=prod*nums[i];
+            if(prod>maxProd){
+                maxProd=prod;
+            }
+            if(prod==0){
+                prod=1;
+            }
+        }
+        return maxProd;
+       
+    }
+}
